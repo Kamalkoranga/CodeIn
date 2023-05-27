f4f75b3e19aac2044a01eb267f478e616df20886
Kamal Koranga <kamalkoranga75@gmail.com>
2023-05-27 16:56:31 +0530

    added comments in auth routes for better understanding

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -5,19 +5,27 @@ from wtforms import ValidationError
 from ..models import User
 
 
+# Define login form fields
 class LoginForm(FlaskForm):
+    # Field for email input
     email = EmailField('Email',
                        validators=[DataRequired(), Email(), Length(1, 64)])
+    # Field for password input
     password = PasswordField('Password', validators=[DataRequired()])
+    # Submit button field
     submit = SubmitField('Sign in')
 
 
+# Define signup form fields
 class RegistrationForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Length(1, 64),
                                              Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Join')
 
+    # Custom validation method for the 'email' field
     def validate_email(self, field):
+        # Check if the email already exists in the User table
         if User.query.filter_by(email=field.data.lower()).first():
+            # Raise a validation error if the email is already registered
             raise ValidationError('Email already registered.')
