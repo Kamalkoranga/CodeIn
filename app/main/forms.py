92fd7bd42ac683c21f85e20c39a3a286c6458aab
Kamal Koranga <kamalkoranga75@gmail.com>
2023-05-27 16:19:22 +0530

    added comments in main routes for better understanding

--- app/main/forms.py
+++ app/main/forms.py
@@ -6,23 +6,42 @@ from wtforms import ValidationError
 
 
 class PostForm(FlaskForm):
+    # Define a form class for creating posts
+    '''
+        Create a TextAreaField named 'body' for the post content.
+        The field is required, meaning it must have a value.
+        The 'render_kw' parameter is used to provide additional attributes to
+        the rendered HTML element.
+        In this case, it adds a placeholder attribute to the field with the
+        specified text.
+    '''
     body = TextAreaField(validators=[DataRequired()], render_kw={"placeholder": "What do you want to talk about?"})
+
+    # Create a FileField named 'post' for uploading a photo
+    # This field allows the user to select a file from their device
     post = FileField('Add a photo')
+
+    # Create a SubmitField named 'submit' for submitting the form
+    # This field represents a button that triggers the form submission
     submit = SubmitField('Post')
 
 
 class EditProfileForm(FlaskForm):
+    # Username field with validators
     username = StringField('Username', validators=[
         DataRequired(), Length(1, 64),
         Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0,
                'Usernames must have only letters, numbers, dots or '
                'underscores')]
     )
+
+    '''Full name and location field with length validator'''
     name = StringField('Full name', validators=[Length(0, 64)])
     location = StringField('Location', validators=[Length(0, 64)])
     about_me = TextAreaField('About me')
-    submit = SubmitField('Submit')
+    submit = SubmitField('Submit')  # submit button
 
     def validate_username(self, field):
+        # Custom validation for the username field
         if User.query.filter_by(username=field.data).first():
             raise ValidationError('Username already in use.')
--- app/main/views.py
+++ app/main/views.py
@@ -9,7 +9,10 @@ from .. import db
 @main.route('/feed', methods=['GET', 'POST'])
 @login_required
 def index():
+    # Create an instance of the PostForm class
     form = PostForm()
+
+    # Check if the form has been submitted and passes form validation
     if form.validate_on_submit():
         post = Post(
             body=form.body.data,
@@ -17,60 +20,115 @@ def index():
             post_data=form.post.data.read(),
             author_id=current_user.id
         )
+
+        # Add the new post to the database session
         db.session.add(post)
+
+        # Commit the changes to the database
         db.session.commit()
+
+        # Display a flash message to indicate successful posting
         flash('Posted Successfully')
+
+        # Redirect the user to the 'index' endpoint
         return redirect(url_for('main.index'))
+
+    # Retrieve all posts from the database in descending order of timestamp
     posts = Post.query.order_by(Post.timestamp.desc()).all()
+
+    '''Render the 'index.html' template, passing the form and posts to the
+    template'''
     return render_template('index.html', form=form, posts=posts)
 
 
 @main.route("/image/<int:id>")
 def get_image(id):
+    """This route is only for retrieving images from the post
+
+    Args:
+        id (int): id of post
+
+    Returns:
+        image/jpeg: returns an image data
+    """
+    # Retrieve the post with the given ID from the database and uses as image
     image = Post.query.filter_by(id=id).first()
+
+    # Return the post's image data as the response (image)
     return image.post_data, {"Content-Type": "image/jpeg"}
 
 
 @main.route('/user/<username>')
 def user(username):
+    # Retrieve the user from the database based on the provided username
     user = User.query.filter_by(username=username).first_or_404()
+
+    # Render the 'user.html' template and pass the user object to the template
     return render_template('user.html', user=user)
 
 
 @main.route('/edit-profile', methods=['GET', 'POST'])
 @login_required
 def edit_profile():
+    # Create an instance of the EditProfileForm
     form = EditProfileForm()
     if form.validate_on_submit():
+
+        # If the form is submitted and passes validation
+        # Update the user's profile details with the form data
         current_user.name = form.name.data
         current_user.username = form.username.data
         current_user.location = form.location.data
         current_user.about_me = form.about_me.data
+
+        # Add the updated user object to the database session and commit the
+        # changes
         db.session.add(current_user._get_current_object())
         db.session.commit()
+
+        # Display a flash message to indicate successful profile update
         flash('Your profile has been updated.')
+
+        # Redirect the user to their profile page
         return redirect(url_for('.user', username=current_user.username))
+
+    # Populate the form fields with the current user's profile data
     form.name.data = current_user.name
     form.location.data = current_user.location
     form.about_me.data = current_user.about_me
     form.username.data = current_user.username
+
+    # Render the 'edit_profile.html' template, passing the form object to the
+    # template
     return render_template('edit_profile.html', form=form)
 
 
 @main.route('/like_post/<post_id>', methods=['POST'])
 @login_required
 def like_post(post_id):
+    # Get the post object based on the provided post_id
     post = Post.query.filter_by(id=int(post_id)).first()
-    like = Like.query.filter_by(author_id=current_user.id, post_id=post.id).first()
+
+    # Check if the user has already liked the post
+    like = Like.query.filter_by(
+        author_id=current_user.id, post_id=post.id).first()
 
     if not post:
         return jsonify({'error': 'Post does not exist.'}, 400)
     elif like:
+        # If the user has already liked the post, remove the like
         db.session.delete(like)
         db.session.commit()
     else:
+        # If the user has not liked the post, create a new like
         like = Like(author_id=current_user.id, post_id=post_id)
         db.session.add(like)
         db.session.commit()
+    res = {
+        # Total number of likes for the post
+        "likes": len(post.likes),
 
-    return jsonify({"likes": len(post.likes), "liked": current_user.id in map(lambda x: x.author_id, post.likes)})
+        # Check if the current user has liked the post
+        "liked": current_user.id in map(lambda x: x.author_id, post.likes)
+    }
+    return jsonify(res)
