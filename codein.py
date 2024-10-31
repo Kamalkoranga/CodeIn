c86a8f5bf45bbae74be4ce4d84d0af7204191e07
Kamal Koranga <kamalkoranga75@gmail.com>
2024-10-31 20:59:44 +0530

    added system status route

--- codein.py
+++ codein.py
@@ -2,10 +2,15 @@ import os
 from app import create_app
 from app import db
 from app.models import User, Post
+from flask import jsonify
 from flask_migrate import Migrate
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 migrate = Migrate(app, db)
+
+@app.route('/system_status', methods=['GET'])
+def system_status():
+    return jsonify({'message': "System is running properly ✅"}), 200
 
 
 # ? For database creation
